22389c22fc99a2b54eae661ef8ef033b775c2af1
julian <iuliusfdp@hotmail.com>
2016-09-28 10:44:06 -0300

    debug false

--- loginuser/settings.py
+++ loginuser/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'or17c50^dchs$$q#4ip)=a24-$9650!9-!%vza9wxfm^voa-q1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
